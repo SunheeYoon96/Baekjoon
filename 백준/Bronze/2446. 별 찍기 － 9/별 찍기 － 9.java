bb39fac38fed9fcbf2561c500990740d7a655e20
SunheeYoon96 <97426905+SunheeYoon96@users.noreply.github.com>
2023-01-24 17:46:01 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 544 ms, Memory: 16484 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	static int N;
+
+	
+	public static void main(String[] args) throws IOException {
+		tokens = new StringTokenizer(input.readLine());
+		N = Integer.parseInt(tokens.nextToken());
+	
+		//백준 2446 : 별 찍기 - 9
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<2*N-1-2*i; j++) {
+				System.out.print("*");
+			}
+//			for(int j=0; j<N-i; j++) {
+//				System.out.print(" ");
+//			}
+			System.out.println();
+		}
+		
+		for(int i=0; i<N-1; i++) {
+			for(int j=0; j<N-(i+2); j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j=0; j<2*(i+1)+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
