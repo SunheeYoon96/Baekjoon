d91c93c706defb15b7969ff498e62edb63412996
SunheeYoon96 <97426905+SunheeYoon96@users.noreply.github.com>
2023-02-26 17:08:39 +0900

    [Gold V] Title: 암호 만들기, Time: 88 ms, Memory: 11800 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -13,7 +13,6 @@ public class Main {
 	static char alphas[];
 	static String line;
 	static String vowels = "aeiou";
-	
 
 	public static void main(String[] args) throws IOException {
 		tokens = new StringTokenizer(input.readLine());
@@ -21,15 +20,15 @@ public class Main {
 		C = Integer.parseInt(tokens.nextToken());
 		
 		alphas = new char[C];
-		//line = input.readLine();
 		tokens = new StringTokenizer(input.readLine());
 		
 		for(char c=0; c<C; c++) {
+			//토큰으로 문자열 받아오기
 			alphas[c] = tokens.nextToken().charAt(0);
 		}
 		
+		//오름차순으로 암호를 만든다고 했으니 조합 전에 미리 정렬해주자.
 		Arrays.sort(alphas);
-		//System.out.println(alphas);
 				
 		makeCombinatoin(0,new char[L], 0);
 		
@@ -37,6 +36,8 @@ public class Main {
 
 	}
 
+    
+	// C개에서 L개추출(조합은 방문체크 X)
 	private static void makeCombinatoin(int nthpick, char choosed[], int sIdx) {
 		if(nthpick == L) {
 			if(checkAlphabet(choosed)) {
@@ -51,20 +52,21 @@ public class Main {
 		}
 		
 	}
+    
 	
+	//만든 조합이 최소한 모음1개 자음2개를 가진 배열인지 판단 
 	private static boolean checkAlphabet(char choosed[]) {
-		int mo = 0;
-		int ja = 0;
+		int mo = 0; //모음 갯수
+		int ja = 0; //자음 갯수
 		
 		for(char c : choosed) {
-			//System.out.println(c);
 			if(vowels.contains(c+"")) {
 				mo++;
 			}else {
 				ja++;
 			}
 		}
-		//System.out.println(ja);
+		
 		if( mo>=1 && ja>=2 ) return true;
 		
 		return false;
