5a0950d61c9e2dea855e8a96c28b5f0db82065e0
SunheeYoon96 <97426905+SunheeYoon96@users.noreply.github.com>
2023-04-06 19:51:45 +0900

    [Gold I] Title: 이항 계수 3, Time: 160 ms, Memory: 42396 KB -BaekjoonHub

---
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	
+	static int T, N, R;
+	static final int mod = 1000000007;
+	static long factorial[], answer;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		tokens = new StringTokenizer(input.readLine());
+		N = Integer.parseInt(tokens.nextToken());
+		R = Integer.parseInt(tokens.nextToken());
+		
+		factorial = new long[N+1];
+		
+		answer = nCr(N, R, mod );
+		
+		System.out.println(answer);
+		
+	}
+
+	private static long nCr(long n, long r, int mod) {
+		if(r==0) return 1L;
+		
+		factorial[0] = 1;
+		
+		for (int i = 1	; i <=N; i++) {
+			factorial[i] = factorial[i-1] *i % mod;
+		}
+		
+		long ans = factorial[N] * pow(factorial[N-R], mod-2, mod)%mod * pow(factorial[R], mod-2, mod)%mod ;
+		
+		return ans;
+	}
+	
+	private static long pow(long base, int exp, int mod) {
+		if(exp==1) return base;
+		if(exp==0) return 1;
+		
+		long res = pow(base, exp/2, mod);
+		res = (res*res)%mod;
+		
+		if(exp%2==1) {
+			res = (res*base)%mod;
+		}
+		
+		return res;
+	}
+
+}
