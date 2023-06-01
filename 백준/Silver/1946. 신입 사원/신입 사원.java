cb24b48da0c5bf350f4a1774502996a85e3a043b
SunheeYoon96 <97426905+SunheeYoon96@users.noreply.github.com>
2023-06-01 22:33:27 +0900

    [Silver I] Title: 신입 사원, Time: 828 ms, Memory: 298848 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,26 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-/**
- * [키워드]
- * 
- * [풀이과정]
- * 
- * [입력]
- * [출력] 
- * 
- * @author 윤선희
- * @since 
- * @see
- * @performance
- * @category #
- */
 
 public class Main {
 	
@@ -28,46 +11,37 @@ public class Main {
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
 	
-	static int TC, N, applicants[][], cnt;
-
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static int TC, N, applicants[][], applicants2[], cnt;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException{
 		TC = Integer.parseInt(input.readLine());
 		
 		for (int tc = 0; tc < TC; tc++) {
 			N = Integer.parseInt(input.readLine());
-			
-			applicants = new int[N][2];
+			applicants2 = new int[N+1];
 			cnt = 1;
 			
 			for (int i = 0; i < N; i++) {
 				tokens = new StringTokenizer(input.readLine());
 				int paper = Integer.parseInt(tokens.nextToken());
 				int meeting = Integer.parseInt(tokens.nextToken());
-				applicants[i][0] = paper;
-				applicants[i][1] = meeting;
+				applicants2[paper] = meeting;
 			}
-
-			//앞 원소 기준 오름차순 정렬
-			Arrays.sort(applicants, (o1, o2) ->{
-				return o1[0]-o2[0];
-			});
 			
-			int minMeeting = applicants[0][1];
+			int meetingScore = applicants2[1];
 			
-			for (int i = 0; i < N; i++) {
-				if(applicants[i][1] < minMeeting) {
+			for (int i = 2; i <= N; i++) {
+				if(meetingScore>applicants2[i]) {
 					cnt++;
-					minMeeting = applicants[i][1]; 
+					meetingScore = applicants2[i];
 				}
 			}
-			
 			output.append(cnt).append("\n");
-			
 		}
 		System.out.println(output);
-
+		
 	}
+
 	
 
 }
