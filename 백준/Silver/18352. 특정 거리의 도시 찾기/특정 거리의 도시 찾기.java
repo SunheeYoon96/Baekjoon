1da10375815b077ebefed32a2b517cc1a5bebaf6
윤선희 <97426905+SunheeYoon96@users.noreply.github.com>
2024-01-21 14:21:33 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1252 ms, Memory: 336004 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder output = new StringBuilder();
+    static StringTokenizer tokens;
+
+    static int N, M, K, X, dist[];
+    static List<List<Integer>> nodes;
+
+    public static void main(String[] args) throws IOException {
+        tokens = new StringTokenizer(input.readLine());
+        N = Integer.parseInt(tokens.nextToken());
+        M = Integer.parseInt(tokens.nextToken());
+        K = Integer.parseInt(tokens.nextToken());
+        X = Integer.parseInt(tokens.nextToken());
+
+        nodes = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            nodes.add(new ArrayList());
+        }
+
+        for (int i = 0; i < M; i++) {
+            tokens = new StringTokenizer(input.readLine());
+            int from = Integer.parseInt(tokens.nextToken());
+            int to = Integer.parseInt(tokens.nextToken());
+            nodes.get(from).add(to);
+        }
+
+        dist = new int[N+1];
+        Arrays.fill(dist, -1);
+
+        bfs(X);
+
+        boolean flag = false;
+        for (int i = 1; i <= N; i++) {
+            if(dist[i]==K) {
+                output.append(i).append("\n");
+                flag = true;
+            }
+        }
+
+        System.out.println(flag ? output : "-1");
+    }
+
+    private static void bfs(int start) {
+        dist[start] = 0;
+        Queue<Integer> que = new ArrayDeque<>();
+        que.add(start);
+
+        while (!que.isEmpty()){
+            int now = que.poll();
+
+            for(int vertex : nodes.get(now)){
+                if (dist[vertex] == -1) { //방문한 적이 없음.
+                    que.add(vertex);
+                    dist[vertex] = dist[now]+1;
+                }
+            }
+        }
+
+    }
+}
