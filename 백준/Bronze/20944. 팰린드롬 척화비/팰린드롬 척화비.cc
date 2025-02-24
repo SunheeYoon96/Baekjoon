981ca154141407767cb79bb06db44f563dcdc42f
윤선희 <97426905+SunheeYoon96@users.noreply.github.com>
2025-02-24 22:11:49 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N;
+    cin >> N;
+    
+    while (N-- > 0) {
+        cout << 'a' ;
+    }
+    
+}
